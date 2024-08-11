b6ffcebd561968c98f345127be8ab2165fd3e644
AdityaRaj140 <17gietucse140@gmail.com>
2024-08-11 18:19:12 +0530

    Updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { RiCopyrightLine } from "react-icons/ri";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#F5F5F5] text-black p-[60px] border rounded-[40px] mt-8">
-      <p>Talup 2023. All rights reserved</p>
-      <div>
-        <a href="#" className="text-gray-400">
+    <footer className="bg-[#F5F5F5] text-black p-[60px] border rounded-[40px] mt-8 flex justify-between items-center">
+      <p className="flex items-center gap-2">
+        <RiCopyrightLine />
+        Talup 2023. All rights reserved
+      </p>
+      <div className="flex items-center gap-8">
+        <a href="#" className="text-gray-700">
           Terms & Conditions
         </a>
-        <span> | </span>
-        <a href="#" className="text-gray-400">
+
+        <a href="#" className="text-gray-700">
           Privacy Policy
         </a>
       </div>
